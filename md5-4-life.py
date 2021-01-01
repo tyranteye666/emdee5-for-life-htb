af0e9a2df72e69aa33181866b480f7ae2e9fc4dd
tyranteye666 <69487631+tyranteye666@users.noreply.github.com>
2021-01-01 11:01:18 -0500

    Update md5-4-life.py

--- md5-4-life.py
+++ md5-4-life.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#takeaways : putting the URL into a variable & making a GET request in another would be faster.
+#takeaways : putting the URL into a variable & making a GET request in another would be faster. or may need to run script a few more times to work (at least for me)
 
 import requests
 import re
@@ -23,11 +23,9 @@ string = nicer_content.split('string')[1].rstrip()
 string = string.encode('utf-8')
 
 #emdee5sum it
-
 md5 = hashlib.md5(string).hexdigest()
 
 # send POST of the hash
 data = dict(hash=md5)
 rpost = req.post("http://206.189.17.51:32142",data=data)
 print(rpost.text)
-
